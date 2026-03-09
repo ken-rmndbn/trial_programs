486fb0cc659610093dc269ca339ab291ec57d61a
ken-rmndbn <remandabankenneth29@gmail.com>
2026-03-09 21:49:12 +0800

    adjusting and creating proper code to get the count of all odd numbers

--- display_odd_numbers.py
+++ display_odd_numbers.py
@@ -1,1 +1,4 @@
 numbers = [float(input(f"Enter number {i}: ")) for i in range (1, 11)]
+odd_numbers = [number for number in numbers if number % 2 != 0]
+count_odd_numbers = len(odd_numbers)
+print("There are",count_odd_numbers, "odd numbers")
