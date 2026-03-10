b6d5d1df73a1e893b833e9a4edb0a732c63c2eb8
ken-rmndbn <remandabankenneth29@gmail.com>
2026-03-10 23:23:02 +0800

    adjusting codes to display not equal

--- display_not_equal_if_two_numbers_are_not_same.py
+++ display_not_equal_if_two_numbers_are_not_same.py
@@ -1,4 +1,6 @@
 number_1 = float(input("Enter first number: "))
 number_2 = float(input("Enter second number: "))
-not_same = number_1 != number_2
-print("Not Equal", not_same)
+if number_1 != number_2:
+    print("Not Equal")
+else:
+    print("Equal")
