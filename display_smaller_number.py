cdeced2793b7c631a56198832369a14b9ec26ea1
ken-rmndbn <remandabankenneth29@gmail.com>
2026-03-10 23:06:56 +0800

    creating conditions to get smaller number

--- display_smaller_number.py
+++ display_smaller_number.py
@@ -1,2 +1,6 @@
 number_1 = float(input("Enter first number: "))
 number_2 = float(input("Enter second number: "))
+if number_1 < number_2:
+    print("First number is the smaller ")
+else:
+    print("Second number is the smaller")
