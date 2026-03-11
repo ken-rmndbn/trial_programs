c740b129eb2a0b6d6ca380bf4cc6b2900af6dcaf
ken-rmndbn <remandabankenneth29@gmail.com>
2026-03-12 00:03:52 +0800

    creating codes to get the result

--- display_result_of_first_number_minus_remaining_numbers.py
+++ display_result_of_first_number_minus_remaining_numbers.py
@@ -1,4 +1,5 @@
 first_number =[1]
 for i in range(1,11):
-    numbers = float(input(f"Enter number {i}"))
-result =
+    numbers = [float(input(f"Enter number {i}: "))]
+result = [first_number - number for number in numbers[2:10]]
+print(result)
