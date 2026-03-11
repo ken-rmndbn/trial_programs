c680fe4c8ccc83ccf8bab495eb2e8c27b0daac8a
ken-rmndbn <remandabankenneth29@gmail.com>
2026-03-12 00:21:41 +0800

    adjusting the codes to apply the difference

--- display_result_of_first_number_minus_remaining_numbers.py
+++ display_result_of_first_number_minus_remaining_numbers.py
@@ -1,5 +1,7 @@
-first_number =[1]
+numbers = []
 for i in range(1,11):
-    numbers = [float(input(f"Enter number {i}: "))]
-result = [first_number - number for number in numbers[2:10]]
-print(result)
+    value = float(input(f"Enter number {i}: "))
+    numbers.append(value)
+first_number = numbers[0]
+result = [first_number - number for number in numbers[1:]]
+print("First number minus the remaining number = ", result)
