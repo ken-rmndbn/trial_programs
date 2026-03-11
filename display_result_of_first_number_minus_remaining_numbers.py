f02e0b33b52a368c66977e1cf7cce5b603f6975c
ken-rmndbn <remandabankenneth29@gmail.com>
2026-03-12 00:25:41 +0800

    creates the final code to get the answer for first number minusthe remaining numbers

--- display_result_of_first_number_minus_remaining_numbers.py
+++ display_result_of_first_number_minus_remaining_numbers.py
@@ -3,5 +3,6 @@ for i in range(1,11):
     value = float(input(f"Enter number {i}: "))
     numbers.append(value)
 first_number = numbers[0]
-result = [first_number - number for number in numbers[1:]]
-print("First number minus the remaining number = ", result)
+for number in numbers[1:]:
+    first_number -= number
+print("First number minus the remaining number = ", first_number)
