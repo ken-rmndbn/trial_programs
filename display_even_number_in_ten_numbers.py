91f3fc3b94b5a439cc4075d3bca31c1a556238ab
ken-rmndbn <remandabankenneth29@gmail.com>
2026-03-12 22:17:39 +0800

    initial commit

--- display_even_number_in_ten_numbers.py
+++ display_even_number_in_ten_numbers.py
@@ -1,2 +1,4 @@
 numbers = [float(input(f"Enter number {i}: ")) for i in range (1, 11)]
 even_numbers = [number for number in numbers if number % 2 == 0]
+count_even_numbers = len(even_numbers)
+print("There are", count_even_numbers, "even numbers")
