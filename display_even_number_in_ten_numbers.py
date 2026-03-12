a921e5c5b292531ca984542678f6a0354e4240ed
ken-rmndbn <remandabankenneth29@gmail.com>
2026-03-12 22:04:52 +0800

    adjusting variable

--- display_even_number_in_ten_numbers.py
+++ display_even_number_in_ten_numbers.py
@@ -1,2 +1,2 @@
 numbers = [float(input(f"Enter number {i}: ")) for i in range (1, 11)]
-odd_numbers = [number for number in numbers if number % 2 == 0]
+even_numbers = [number for number in numbers if number % 2 == 0]
