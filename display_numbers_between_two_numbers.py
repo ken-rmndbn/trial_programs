992af809fcd0217efe14dc3a664d42d8a407bd05
ken-rmndbn <remandabankenneth29@gmail.com>
2026-03-12 23:08:36 +0800

    creating and adjusting codes to get the numbers between two number

--- display_numbers_between_two_numbers.py
+++ display_numbers_between_two_numbers.py
@@ -1,2 +1,12 @@
 number_1 = float(input("Enter first number: "))
 number_2 = float(input("Enter second number: "))
+if number_1 < number_2:
+    starting_number = number_1 + 1
+    while starting_number < number_2:
+        print(starting_number)
+        starting_number += 1
+else:
+    starting_number = number_1 - 1
+    while starting_number > number_2:
+        print(starting_number)
+        starting_number -= 1
