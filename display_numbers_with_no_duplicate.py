348603d85358b921e45ae8204f6f8df5d533c1d5
ken-rmndbn <remandabankenneth29@gmail.com>
2026-03-17 22:29:42 +0800

    adusting code to get numbers with no duplicate

--- display_numbers_with_no_duplicate.py
+++ display_numbers_with_no_duplicate.py
@@ -1,2 +1,3 @@
 numbers = [float(input(f"Enter number {i}: ")) for i in range (1, 11)]
-unique_number = [number for number in numbers if numbers.count == 1]
+unique_number = [number for number in numbers if numbers.count(number) == 1]
+print("Numbers with no duplicates: ", unique_number if unique_number else "None")
