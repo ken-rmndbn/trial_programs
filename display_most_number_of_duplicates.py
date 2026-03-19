fe7585044071a45d166811f4830951b38f394106
ken-rmndbn <remandabankenneth29@gmail.com>
2026-03-19 23:27:25 +0800

    creating codes to get the most duplicate number

--- display_most_number_of_duplicates.py
+++ display_most_number_of_duplicates.py
@@ -7,5 +7,11 @@ while True:
         numbers.append(value)
     except ValueError:
         break
-
-
+if numbers:
+    counts = Counter(numbers)
+    most_common_data = counts.most_common(1)[0]
+    number, frequency = most_common_data
+    if frequency > 1:
+        print(f"\nThe number with the most duplicate is {numbers} (appeared {frequency} times).")
+    else:
+        print("\nNo duplicates found:")
