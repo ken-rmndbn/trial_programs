f294027918a2d71aab2acd513249a8487013b494
ken-rmndbn <remandabankenneth29@gmail.com>
2026-03-19 22:59:43 +0800

    adjusting codes to get the duplicate of ten numbers

--- display_duplicate_of_ten_numbers.py
+++ display_duplicate_of_ten_numbers.py
@@ -1,6 +1,6 @@
 duplicates = []
 numbers = [float(input(f"Enter number {i}: ")) for i in range (1, 11)]
-for x in numbers:
-    if numbers.count(x) > 1 and x is not in duplicates:
-        duplicates.append(x)
+for duplicate in numbers:
+    if numbers.count(duplicate) > 1 and duplicate not in duplicates:
+        duplicates.append(duplicate)
 print("Duplicates: ", duplicates)
