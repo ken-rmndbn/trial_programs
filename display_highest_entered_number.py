95616bdb359432c4fa89f1ed698ce34d39e55fdb
ken-rmndbn <remandabankenneth29@gmail.com>
2026-03-19 23:41:18 +0800

    creating condition to get the highest entered number

--- display_highest_entered_number.py
+++ display_highest_entered_number.py
@@ -6,3 +6,8 @@ while True:
         numbers.append(value)
     except ValueError:
         break
+if numbers:
+    highest_number = max(numbers)
+    print("The highest number you entered is:", highest_number)
+else:
+    print("Invalid input")
