f167bb2f4a79808684b998dfc29d442ef07f3d80
ken-rmndbn <remandabankenneth29@gmail.com>
2026-03-19 22:32:44 +0800

    creating code to get numbers from lowest to highest

--- display_numbers_from_lowest_to_highest.py
+++ display_numbers_from_lowest_to_highest.py
@@ -2,8 +2,14 @@ numbers = []
 while True:
     user_input = input("Enter a number: ")
     try:
-        number = float(user_input)
+        value = float(user_input)
+        numbers.append(value)
     except ValueError:
         print("Invalid Input")
         break
-        
+if numbers:
+    numbers.sort()
+    print("Numbers from lowest to highest")
+    print(numbers)
+else:
+    print("No numbers entered")
