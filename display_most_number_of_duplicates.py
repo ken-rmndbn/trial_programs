79ad5b03422b8c25d5ad8631a2acd4eeae873f47
ken-rmndbn <remandabankenneth29@gmail.com>
2026-03-19 23:17:07 +0800

    creating condition to detect invalid inputs

---
+++ display_most_number_of_duplicates.py
@@ -0,0 +1,11 @@
+from collections import Counter
+numbers = []
+while True:
+    user_input = input("Enter Number: ")
+    try:
+        value = float(user_input)
+        numbers.append(value)
+    except ValueError:
+        break
+
+
