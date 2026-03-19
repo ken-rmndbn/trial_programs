3f1e756adb5376b40dbb9133d420beadb7defb6a
ken-rmndbn <remandabankenneth29@gmail.com>
2026-03-19 21:41:26 +0800

    creating condition to get unique and remove duplicate numbers

--- display_unique_and_duplicate.py
+++ display_unique_and_duplicate.py
@@ -3,3 +3,8 @@ while True:
     if not user_input.isdigit():
         print("Invalid input")
         break
+    unique_digits = set(user_input)
+    if len(unique_digits) == len(user_input):
+        print("Unique:")
+    else:
+        print("Duplicate")
