7e74b5faae8dfaf5ee3f3d4535c745020ba723b8
ken-rmndbn <remandabankenneth29@gmail.com>
2026-03-21 01:35:36 +0800

    creating code to swap letters in text without swapcase function

--- display_same_functionality_without_swapcase_function.py
+++ display_same_functionality_without_swapcase_function.py
@@ -1,3 +1,9 @@
 def main():
     user_input = input("Enter text you want to swap: ")
-    
+    swapped = "".join([
+        character.lower() if character.isupper() else character.upper()
+        for character in user_input
+    ])
+    print("Result: ", swapped)
+if __name__ == "__main__":
+    main()
