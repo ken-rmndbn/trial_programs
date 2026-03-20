e628b3f1561d3ce6a78632b34542d21b0bcfc05d
ken-rmndbn <remandabankenneth29@gmail.com>
2026-03-20 22:22:40 +0800

    creating code to add space to entered text without using ljust function

--- display_same_functionality_without_ljust_function.py
+++ display_same_functionality_without_ljust_function.py
@@ -4,3 +4,8 @@ try:
 except ValueError:
     print("Please enter a valid number")
     exit()
+space_to_add = target_width - len(user_input)
+final_string = user_input + (" " * space_to_add)
+print("Original Length: ", len(user_input))
+print("Result: ", final_string)
+print("New Length: ", len(final_string))
