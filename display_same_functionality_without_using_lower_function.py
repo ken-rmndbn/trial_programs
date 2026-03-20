a6d562abaac86070a6b050baf8dc0a2a32681390
ken-rmndbn <remandabankenneth29@gmail.com>
2026-03-20 21:25:58 +0800

    creating and adjusting code to get the lowercase of the text

--- display_same_functionality_without_using_lower_function.py
+++ display_same_functionality_without_using_lower_function.py
@@ -1,4 +1,10 @@
 text = input("Enter any word or text: ")
 result = ""
 for character in text:
-    if "A"
+    if "A" <= character <= "Z":
+        lower_character = chr(ord(character) + 32)
+        result += lower_character
+    else:
+        result += character
+print("Original: ", text)
+print("Lowercase: ", result)
