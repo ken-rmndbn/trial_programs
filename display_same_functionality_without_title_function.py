b645844c30abe6a689f688394a61e7b5eead2801
ken-rmndbn <remandabankenneth29@gmail.com>
2026-03-21 01:51:00 +0800

    creating code to get the first letter capitalization of text or phrases without using title function

--- display_same_functionality_without_title_function.py
+++ display_same_functionality_without_title_function.py
@@ -1,2 +1,3 @@
 user_input = input("Enter Text: ")
-result = " ".join([word[0]])
+result = " ".join([word[0].upper() + word[1:].lower() for word in user_input.split()])
+print("First letter text capitalization: ", result)
