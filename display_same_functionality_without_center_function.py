04989ebbca9ab0bf1447cd00b6c967d10eedab49
ken-rmndbn <remandabankenneth29@gmail.com>
2026-03-20 22:53:17 +0800

    creating code to manually center the text without using center function

--- display_same_functionality_without_center_function.py
+++ display_same_functionality_without_center_function.py
@@ -5,3 +5,11 @@ def manual_center(text, width):
     left_side = total_padding // 2
     right_side = total_padding - left_side
     return (" " * left_side) + text + (" " * right_side)
+user_text = input("Enter Text: ")
+try:
+    user_width = int(input("Enter total width in number: "))
+    centered_result = manual_center(user_text, user_width)
+    print("Result:", centered_result)
+    print("Total length: ", len(centered_result), "characters")
+except ValueError:
+    print("Error! Please enter valid number for width")
